06bcd05da5f62f6d371a7e94b3c1cd6f1de080a9
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-07-14 21:52:13 -0600

    Bad lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
+from markupsafe import escape, Markup
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:hello>')
+def print_string(hello):
+    print("hello")
+    return Markup.escape(hello)
+
+@app.route('/count/<int:count>')
+def count(count):
+    count_range = range(0, 10)
+    count_string = '\n'.join(str(i) for i in count_range)
+    return Markup.escape(count_string)
+
+
+@app.route('/math/<int:num1>/<op>/<int:num2>')
+def math(num1, op, num2):
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == 'div':
+        result = num1 / num2
+    elif op == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation", 400
+
+    return str(result)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -42,7 +42,7 @@ class TestApp:
     def test_count_range_10(self):
         '''counts through range of parameter in "/count/<parameter" on separate lines.'''
         response = app.test_client().get('/count/10')
-        count = '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n'
+        count = '0\n1\n2\n3\n4\n5\n6\n7\n8\n9'
         assert(response.data.decode() == count)
 
     def test_math_route(self):
@@ -69,7 +69,7 @@ class TestApp:
         '''divides parameters in "/math/" resource when operation is "div".'''
         response = app.test_client().get('/math/5/div/5')
         assert(response.data.decode() == '1.0')
-    
+
     def test_math_modulo(self):
         '''finds remainder of parameters in "/math/" resource when operation is "%".'''
         response = app.test_client().get('/math/5/%/5')
